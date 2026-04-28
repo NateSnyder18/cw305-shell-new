5eeeabfab92df86ca6c9f6824064a4ccd4c11fbd
unknown <dek226@lehigh.edu>
2026-04-28 15:01:19 -0400

    push generated ip cores and chaned ip repo to hardcode device type

--- examples/aes128_hls/create_hls_ip.sh
+++ examples/aes128_hls/create_hls_ip.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-vitis_hls -f ../ip_repo/aes128_hls/create_ip.tcl target-board=cw305 target-freq=20MHz
+vitis_hls -f ../ip_repo/aes128_hls/create_ip.tcl 
+#vitis_hls -f ../ip_repo/aes128_hls/create_ip.tcl -- target-board=cw305
+#vitis_hls -f ../ip_repo/aes128_hls/create_ip.tcl -tclargs "target-board=cw305"
